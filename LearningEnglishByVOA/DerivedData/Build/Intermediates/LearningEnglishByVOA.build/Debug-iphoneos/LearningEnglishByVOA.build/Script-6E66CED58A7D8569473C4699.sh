a401012df815aa8ebc7e8e4de1ac72a7acc04fb6
XIANGWEI WANG <xiangwei.wang@dji.com>
2017-05-17 15:46:41 +0900

    commit

---
+++ LearningEnglishByVOA/DerivedData/Build/Intermediates/LearningEnglishByVOA.build/Debug-iphoneos/LearningEnglishByVOA.build/Script-6E66CED58A7D8569473C4699.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-LearningEnglishByVOA/Pods-LearningEnglishByVOA-frameworks.sh"
+
---
+++ LearningEnglishByVOA/DerivedData/Build/Intermediates/Pods.build/Debug-iphoneos/iOS-Slide-Menu.build/DerivedSources/iOS_Slide_Menu_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char iOS_Slide_MenuVersionString[];
+ extern const double iOS_Slide_MenuVersionNumber;
+
+ const unsigned char iOS_Slide_MenuVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:iOS_Slide_Menu  PROJECT:Pods-1" "\n";
+ const double iOS_Slide_MenuVersionNumber __attribute__ ((used)) = (double)1.;
