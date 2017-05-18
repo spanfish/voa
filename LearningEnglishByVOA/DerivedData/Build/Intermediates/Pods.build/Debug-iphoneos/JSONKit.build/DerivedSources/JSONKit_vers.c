74f90e2bfae99c16210be41c67e5cdb65f8ad732
XIANGWEI WANG <xiangwei.wang@dji.com>
2017-05-18 09:53:55 +0900

    commit

--- LearningEnglishByVOA/DerivedData/Build/Intermediates/Pods.build/Debug-iphoneos/JSONKit.build/DerivedSources/JSONKit_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char JSONKitVersionString[];
- extern const double JSONKitVersionNumber;
-
- const unsigned char JSONKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:JSONKit  PROJECT:Pods-1" "\n";
- const double JSONKitVersionNumber __attribute__ ((used)) = (double)1.;
